4117bd39774637dadbcec850fba95e6798b9a1f8
scuderia1000 <a4cbepk@gmail.com>
2020-08-28 08:03:12 +0500

    add login component

---
+++ src/components/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Login extends React.Component{
+    render(): React.ReactNode {
+        return (
+            <div className={"login-form"}>
+                Login
+            </div>
+        );
+    }
+}
+
+export default Login;
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import {configureStore} from "./store/configureStore";
 
 const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    </Provider>,
+    document.getElementById('root')
 );
